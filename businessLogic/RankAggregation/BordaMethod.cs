7952038857f284d23678141708cd21ad03400f6b
CodeValue <CodeValue@DESKTOP-2KK2JJV1>
2016-12-26 13:26:46 +0200

    check the decription in borda

--- businessLogic/RankAggregation/BordaMethod.cs
+++ businessLogic/RankAggregation/BordaMethod.cs
@@ -10,39 +10,95 @@ namespace businessLogic.RankAggregation
 {
     internal class BordaMethod
     {
-         public List<FinalResult> BordaRank(IEnumerable<SearchEngineResultsList> allSearchResults)
-         {
+        public List<FinalResult> BordaRank(IEnumerable<SearchEngineResultsList> allSearchResults)
+        {
             var aggregationResults = new Dictionary<string, FinalResult>();
-             foreach (var searchResults in allSearchResults)
-             {
+            //var descriptionDictionary = new Dictionary<string, FinalResult>();
+            foreach (var searchResults in allSearchResults)
+            {
                 foreach (var result in searchResults.Results)
-                 {
-                     if (!aggregationResults.ContainsKey(result.DisplayUrl))
-                     {
-                         aggregationResults.Add(result.DisplayUrl,new FinalResult
-                         {
-                             DisplayUrl = result.DisplayUrl,
-                             Description = result.Description,
-                             Title = result.Title
-                         });
-                     }
+                {
+                    var key = result.DisplayUrl;
+                    var fl = false;
 
-                     if (aggregationResults[result.DisplayUrl].Description == null)
-                     {
-                         aggregationResults[result.DisplayUrl].Description = result.Description;
-                     }
+                    foreach (
+                        var ar in
+                        aggregationResults.Values.Where(
+                            ar => CheckMatch(ar.DisplayUrl, result.DisplayUrl, ar.Description, result.Description)))
+                    {
+                        key = ar.DisplayUrl;
+                        fl = true;
+                        break;
+                    }
 
-                    aggregationResults[result.DisplayUrl].Rank += 100 - result.Rank;
-                    aggregationResults[result.DisplayUrl].SearchEngines.Add(searchResults.SearchEngineName,(int)result.Rank);
-                 }
-             }
+                    if (!fl && !aggregationResults.ContainsKey(key))
+                    {
+                        aggregationResults.Add(key, new FinalResult
+                        {
+                            DisplayUrl = result.DisplayUrl,
+                            Description = result.Description,
+                            Title = result.Title
+                        });
+                    }
 
-             var orderedResults = from result in aggregationResults.Values
-                                  orderby result.Rank descending
-                                  select result;
+                    if (aggregationResults[key].Description == null)
+                    {
+                        aggregationResults[key].Description = result.Description;
+                    }
+                    aggregationResults[key].Rank += 100 - result.Rank;
+                    aggregationResults[key].SearchEngines.Add(searchResults.SearchEngineName, (int) result.Rank);
+                }
+            }
 
-             var x = orderedResults.ToList();
-             return orderedResults.ToList();
-         }
+            var orderedResults = from result in aggregationResults.Values
+                orderby result.Rank descending
+                select result;
+
+            var x = orderedResults.ToList();
+            return orderedResults.ToList();
+        }
+
+        private bool CheckMatch(string arUrl, string resultUrl, string description1, string description2)
+        {
+            return CheckUrl(arUrl, resultUrl) && CheckMatch(description1, description2);
+        }
+
+        private bool CheckUrl(string arUrl, string resultUrl)
+        {
+            try
+            {
+                if (arUrl.Equals(resultUrl)) return false;
+                var ar = arUrl.IndexOf("/", StringComparison.Ordinal);
+                var res = resultUrl.IndexOf("/", StringComparison.Ordinal);
+                if (arUrl.Substring(0, ar).Equals(resultUrl.Substring(0, res)))
+                {
+                    return true;
+                }
+            }
+            catch (Exception)
+            {
+                
+                return false;
+            }
+            return false;
+        }
+
+        private bool CheckMatch(string results, string resultDescription)
+        {
+            if (resultDescription==null)
+            {
+                return false;
+            }
+
+            var length = 15;
+                if (resultDescription.Length< length)
+                {
+                    length = resultDescription.Length;
+                }
+                
+                return results.Contains(resultDescription.Substring(0, length));
+        }
+
+       
     }
 }
